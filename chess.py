394eca6c3d1c33d3ac229334c6859182b044aada
prabhakaran9397 <prabhakaran9397@gmail.com>
2016-12-10 16:37:25 +0530

    pieces are placed at start

--- chess.py
+++ chess.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 	
-#I need Board
+#I need Board - X
 #I need Pieces
 #I need Players
 #I need Rules
@@ -13,8 +13,10 @@ class ChessBoard:
 		self.Texture 	= "#"
 		self.Boxes		= 8
 		self.Height		= Height
-		self.White		= "\033[98m{}\033[00m" # Bash Black
-		self.Black		= "\033[91m{}\033[00m" # Bash Red
+		self.White		= "\033[97m{}\033[00m" 
+		self.Black		= "\033[90m{}\033[00m" 
+		self.WhiteP		= "\033[107m{}\033[00m"
+		self.BlackP		= "\033[100m{}\033[00m"
 		self.Board 		= []
 		for Brow in range(self.Boxes*self.Height):
 			Row = []
@@ -30,15 +32,38 @@ class ChessBoard:
 					else:
 						Row.append(self.White.format(self.Texture))
 			self.Board.append(Row)
+		self.ResetBoard()
 
-	def set(self, i, j, Piece, Color):
+	def Set(self, i, j, Piece, Color):
 		# 0 -> Black; 1 -> White
 		if Color:
 			self.Board[self.Height*i+(self.Height-1)/2][self.Height*j+(self.Height-1)/2] = \
-			self.White.format(Piece[0])
+			self.WhiteP.format(Piece[0])
 		else:
 			self.Board[self.Height*i+(self.Height-1)/2][self.Height*j+(self.Height-1)/2] = \
-			self.Black.format(Piece[0])
+			self.BlackP.format(Piece[0])
+
+	def ResetBoard(self):
+		self.Set(0, 0, "R", 0)
+		self.Set(0, 1, "B", 0)
+		self.Set(0, 2, "H", 0)
+		self.Set(0, 3, "K", 0)
+		self.Set(0, 4, "Q", 0)
+		self.Set(0, 5, "H", 0)
+		self.Set(0, 6, "B", 0)
+		self.Set(0, 7, "R", 0)
+		for i in range(8):
+			self.Set(1, i, "6", 0)
+		self.Set(7, 0, "R", 1)
+		self.Set(7, 1, "B", 1)
+		self.Set(7, 2, "H", 1)
+		self.Set(7, 3, "Q", 1)
+		self.Set(7, 4, "K", 1)
+		self.Set(7, 5, "H", 1)
+		self.Set(7, 6, "B", 1)
+		self.Set(7, 7, "R", 1)
+		for i in range(8):
+			self.Set(6, i, "6", 1)
 
 	def PrintBoard(self, Space=20):
 		print
@@ -52,5 +77,4 @@ class ChessBoard:
 
 if __name__ == '__main__':
 	CBoard = ChessBoard(3)
-	CBoard.set(0, 3, "K", 1)
 	CBoard.PrintBoard()
